a7b8cf55a623d2a58f16b400bbe19ca7ac772190
Taylor Sutton <taylor.sutton@eagles.ewu.edu>
2016-02-28 16:15:31 -0800

    first to gh

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* JS Linked at 
+
+cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+    jquery
+
+http://codepen.io/taylorsutton/pen/pyoGpj.js
+    SimpleWeather.js 
+
+http://codepen.io/taylorsutton/pen/BKaMWM.js
+    My GetWeatherInfo function and Call
+
+*/
